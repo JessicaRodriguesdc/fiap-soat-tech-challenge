fe5a6afd5d0ac103b17feced61cc07140b8b1094
DiguyaDeveloper <diegocecconcwb@outlook.com>
2024-07-27 19:41:39 -0300

    feat: :sparkles: add search all products by UUID list

--- src/main/java/br/com/fiap/tech_challenge/core/domain/ports/ProductPersistence.java
+++ src/main/java/br/com/fiap/tech_challenge/core/domain/ports/ProductPersistence.java
@@ -2,8 +2,13 @@ package br.com.fiap.tech_challenge.core.domain.ports;
 
 import br.com.fiap.tech_challenge.core.domain.models.Product;
 
+import java.util.List;
+import java.util.UUID;
+
 public interface ProductPersistence {
 
     Product create(Product product);
+
+    List<Product> findAllByIds(List<UUID> ids);
 
 }
