5558ba885e111acb8b1e7e5fb34a771a8ea1d563
DiguyaDeveloper <diegocecconcwb@outlook.com>
2024-07-30 20:21:01 -0300

    refactor: :recycle: Update order path removed /api

--- src/main/java/br/com/fiap/tech_challenge/adapters/driver/api/controller/OrderController.java
+++ src/main/java/br/com/fiap/tech_challenge/adapters/driver/api/controller/OrderController.java
@@ -10,7 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/orders")
+@RequestMapping("/v1/orders")
 public class OrderController {
 
     private final CreateOrderUseCase createOrderUseCase;
