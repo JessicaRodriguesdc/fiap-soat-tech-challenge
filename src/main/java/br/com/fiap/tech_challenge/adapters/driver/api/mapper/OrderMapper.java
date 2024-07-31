5b03bfb780f4ac4f75db11f41506bc636ccf3bfb
DiguyaDeveloper <diegocecconcwb@outlook.com>
2024-07-31 00:07:26 -0300

    feat: :sparkles: Transform request type from UUID to String and Add validation NullOrValidUUID

--- src/main/java/br/com/fiap/tech_challenge/adapters/driver/api/mapper/OrderMapper.java
+++ src/main/java/br/com/fiap/tech_challenge/adapters/driver/api/mapper/OrderMapper.java
@@ -4,14 +4,16 @@ import br.com.fiap.tech_challenge.adapters.driver.api.dto.CreateOrderRequestDTO;
 import br.com.fiap.tech_challenge.core.domain.usecases.order.dto.CreateOrderDTO;
 import org.springframework.stereotype.Component;
 
+import java.util.UUID;
+
 @Component
 public class OrderMapper {
 
     public CreateOrderDTO toCreateOrder(CreateOrderRequestDTO dto) {
         return new CreateOrderDTO(
-                dto.customerId(),
+                dto.customerId() != null ? UUID.fromString(dto.customerId()) : null,
                 dto.products().stream().map(orderProducts -> new CreateOrderDTO.OrderProducts(
-                        orderProducts.id(),
+                        UUID.fromString(orderProducts.id()),
                         orderProducts.observation()
                 )).toList()
         );
