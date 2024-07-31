77c6aeda543a89d34c2bde4571f79f1cd5aa55f4
DiguyaDeveloper <diegocecconcwb@outlook.com>
2024-07-31 00:06:56 -0300

    feat: :sparkles: Add not null in fake checkout request

--- src/main/java/br/com/fiap/tech_challenge/adapters/driver/api/dto/FakeCheckoutRequestDTO.java
+++ src/main/java/br/com/fiap/tech_challenge/adapters/driver/api/dto/FakeCheckoutRequestDTO.java
@@ -2,9 +2,10 @@ package br.com.fiap.tech_challenge.adapters.driver.api.dto;
 
 import br.com.fiap.tech_challenge.adapters.driver.api.validator.ValidUUID;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record FakeCheckoutRequestDTO(
-        @ValidUUID String orderId,
+        @ValidUUID @NotNull String orderId,
         @NotBlank String qrCode
 ) {
 }
