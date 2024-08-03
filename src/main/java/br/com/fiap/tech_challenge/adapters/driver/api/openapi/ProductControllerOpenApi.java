ea40d5f179fed072896854923adb727ffe444ed8
Rodrigo Sartori <rodrigo.sartori00@yahoo.com>
2024-08-03 00:40:06 -0300

    feature: definition of interface

--- src/main/java/br/com/fiap/tech_challenge/adapters/driver/api/openapi/ProductControllerOpenApi.java
+++ src/main/java/br/com/fiap/tech_challenge/adapters/driver/api/openapi/ProductControllerOpenApi.java
@@ -3,13 +3,17 @@ package br.com.fiap.tech_challenge.adapters.driver.api.openapi;
 import br.com.fiap.tech_challenge.adapters.driver.api.dto.ProductRequestDTO;
 import br.com.fiap.tech_challenge.adapters.driver.api.dto.ProductResponseDTO;
 import br.com.fiap.tech_challenge.adapters.driver.api.handler.ErrorsValidateData;
+import br.com.fiap.tech_challenge.core.domain.models.enums.CategoryProductEnum;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+
+import java.util.UUID;
 
 @Tag(name = "Product")
 public interface ProductControllerOpenApi {
@@ -20,4 +24,14 @@ public interface ProductControllerOpenApi {
         @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = ErrorsValidateData.class))))
     @ApiResponse(responseCode = "500", description = "Internal Server Error Response", content = @Content(mediaType = "application/json", schema = @Schema(ref = "ProblemDto")))
     ResponseEntity<ProductResponseDTO> create(ProductRequestDTO productDTO);
+
+
+    ResponseEntity<Page<ProductResponseDTO>> getProductsByCategory(CategoryProductEnum category, int page, int size);
+
+
+
+    ResponseEntity<ProductResponseDTO> update(final UUID id, ProductRequestDTO productDTO);
+
+
+    ResponseEntity<?> deleteProductById(UUID id);
 }
