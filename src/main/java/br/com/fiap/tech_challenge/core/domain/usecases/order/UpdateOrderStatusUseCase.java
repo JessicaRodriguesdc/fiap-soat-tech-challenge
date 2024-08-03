818d60eb69d10a09258eabd8cd38fafd77872649
Jéssica Rodrigues <jessicarodridc@gmail.com>
2024-08-03 16:19:39 -0300

    fix: ♻️ Renaming class enum OrderStatus to StatusOrderEnum

--- src/main/java/br/com/fiap/tech_challenge/core/domain/usecases/order/UpdateOrderStatusUseCase.java
+++ src/main/java/br/com/fiap/tech_challenge/core/domain/usecases/order/UpdateOrderStatusUseCase.java
@@ -1,10 +1,9 @@
 package br.com.fiap.tech_challenge.core.domain.usecases.order;
 
-import br.com.fiap.tech_challenge.core.domain.models.Order;
-import br.com.fiap.tech_challenge.core.domain.models.enums.OrderStatus;
+import br.com.fiap.tech_challenge.core.domain.models.enums.StatusOrderEnum;
 
 import java.util.UUID;
 
 public interface UpdateOrderStatusUseCase {
-    void updateStatusById(OrderStatus status, UUID id);
+    void updateStatusById(StatusOrderEnum status, UUID id);
 }
