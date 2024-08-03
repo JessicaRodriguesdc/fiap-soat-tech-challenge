3698cd44dd256ff5f39f8d12814dde311f8906d8
Rodrigo Sartori <rodrigo.sartori00@yahoo.com>
2024-08-03 01:47:51 -0300

    refactor: change the way to pageable Orders

--- src/main/java/br/com/fiap/tech_challenge/adapters/driver/api/controller/OrderController.java
+++ src/main/java/br/com/fiap/tech_challenge/adapters/driver/api/controller/OrderController.java
@@ -2,24 +2,22 @@ package br.com.fiap.tech_challenge.adapters.driver.api.controller;
 
 import br.com.fiap.tech_challenge.adapters.driver.api.dto.CreateOrderRequestDTO;
 import br.com.fiap.tech_challenge.adapters.driver.api.dto.CreateOrderResponseDTO;
-import br.com.fiap.tech_challenge.adapters.driver.api.dto.PaidOrdersPaginatedResponseDTO;
+import br.com.fiap.tech_challenge.adapters.driver.api.dto.OrderSummaryResponseDTO;
 import br.com.fiap.tech_challenge.adapters.driver.api.mapper.OrderMapper;
-import br.com.fiap.tech_challenge.core.domain.models.Order;
+import br.com.fiap.tech_challenge.adapters.driver.api.openapi.OrderControllerOpenApi;
 import br.com.fiap.tech_challenge.core.domain.models.enums.OrderStatus;
 import br.com.fiap.tech_challenge.core.domain.usecases.order.CreateOrderUseCase;
 import br.com.fiap.tech_challenge.core.domain.usecases.order.FindPaidOrdersUseCase;
 import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("/v1/orders")
-public class OrderController {
+public class OrderController implements OrderControllerOpenApi {
 
     private final CreateOrderUseCase createOrderUseCase;
     private final FindPaidOrdersUseCase findPaidOrdersUseCase;
@@ -33,22 +31,22 @@ public class OrderController {
         this.mapper = mapper;
     }
 
+    @Override
     @GetMapping
-    public ResponseEntity<PaidOrdersPaginatedResponseDTO> findAllIsPaidOrders(@RequestParam("status") OrderStatus status,
-                                                                              Pageable pageable) {
+    public ResponseEntity<Page<OrderSummaryResponseDTO>> findAllIsPaidOrders(@RequestParam("status") OrderStatus status,
+                                                                              @RequestParam(required = false, defaultValue = "0") int page,
+                                                                              @RequestParam(required = false, defaultValue = "10") int size) {
 
-        Page<Order> ordersPage = findPaidOrdersUseCase.findAllPaidOrders(status, pageable);
+        var orders = findPaidOrdersUseCase
+                .findAllPaidOrders(status, PageRequest.of(page, size))
+                .map(OrderSummaryResponseDTO::new);
 
-        List<PaidOrdersPaginatedResponseDTO.OrderSummary> content = ordersPage.getContent().stream()
-                .map(PaidOrdersPaginatedResponseDTO.OrderSummary::new).toList();
-
-        PaidOrdersPaginatedResponseDTO response = new PaidOrdersPaginatedResponseDTO(content, pageable);
-
-        return ResponseEntity.status(HttpStatus.OK).body(response);
+        return ResponseEntity.status(HttpStatus.OK).body(orders);
     }
 
+    @Override
     @PostMapping
-    private ResponseEntity<CreateOrderResponseDTO> create(@RequestBody @Valid CreateOrderRequestDTO orderRequest){
+    public ResponseEntity<CreateOrderResponseDTO> create(@RequestBody @Valid CreateOrderRequestDTO orderRequest){
         var mapperCreateOrder = mapper.toCreateOrder(orderRequest);
         var order = createOrderUseCase.create(mapperCreateOrder);
         var response = new CreateOrderResponseDTO(order.getId(), order.getPaymentId());
--- src/main/java/br/com/fiap/tech_challenge/config/openapi/schemas/PageableSortResponseDTO.java
+++ src/main/java/br/com/fiap/tech_challenge/config/openapi/schemas/PageableSortResponseDTO.java
@@ -2,7 +2,7 @@ package br.com.fiap.tech_challenge.config.openapi.schemas;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
-public record PageableSortProductResponseDTO (
+public record PageableSortResponseDTO(
         @Schema(example = "true") Boolean empty,
         @Schema(example = "false") Boolean sorted,
         @Schema(example = "true") Boolean unsorted
