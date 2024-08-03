a4c7570a5dca22de65aec29c7021748183e50ec0
Rodrigo Sartori <rodrigo.sartori00@yahoo.com>
2024-08-03 01:07:03 -0300

    feature: create DTOs to Swagger

---
+++ src/main/java/br/com/fiap/tech_challenge/config/openapi/schemas/PageableProductResponseDTO.java
@@ -0,0 +1,24 @@
+package br.com.fiap.tech_challenge.config.openapi.schemas;
+
+import br.com.fiap.tech_challenge.adapters.driver.api.dto.ProductResponseDTO;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+import java.util.List;
+
+public record PageableProductResponseDTO(
+        @Schema(example = "1") Long totalPages,
+        @Schema(example = "1") Long totalElements,
+        @Schema(example = "10") Long size,
+        List<ProductResponseDTO> content,
+        @Schema(example = "0") Long number,
+        PageableSortProductResponseDTO sort,
+        @Schema(example = "true") Boolean first,
+        @Schema(example = "true") Boolean last,
+        @Schema(example = "1") Long numberOfElements,
+        PageablePageableProductResponseDTO pageable,
+        @Schema(example = "false") Boolean empty
+) {
+}
+
+
+
