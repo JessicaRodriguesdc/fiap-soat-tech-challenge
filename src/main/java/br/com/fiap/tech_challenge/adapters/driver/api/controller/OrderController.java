3c588b84580e9a2a38eaa3c87c6af567b93fc07a
Rodrigo Sartori <rodrigo.sartori00@yahoo.com>
2024-08-09 19:51:18 -0300

    refactor: replace OrderPage to var

--- src/main/java/br/com/fiap/tech_challenge/adapters/driver/api/controller/OrderController.java
+++ src/main/java/br/com/fiap/tech_challenge/adapters/driver/api/controller/OrderController.java
@@ -37,7 +37,7 @@ public class OrderController implements OrderControllerOpenApi {
 			@RequestParam("isPaid") Boolean isPaid, @RequestParam(required = false, defaultValue = "0") int page,
 			@RequestParam(required = false, defaultValue = "10") int size) {
 
-		OrderPage pageableOrder = findPaidOrdersUseCase.findByIsPaidAndStatus(status, isPaid, page, size);
+		var pageableOrder = findPaidOrdersUseCase.findByIsPaidAndStatus(status, isPaid, page, size);
 
 		return ResponseEntity.status(HttpStatus.OK).body(new OrderPageResponseDTO(pageableOrder));
 	}
